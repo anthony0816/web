e872c7983a75a96694d8c4e9270e91235aeebb9f
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-06-12 10:09:52 -0400

    Trabajando en las funcionalidades de album

--- modal_menu_album.js
+++ modal_menu_album.js
@@ -1,4 +1,5 @@
 import { supabase } from "./script.js";
+import { extraerNumeros } from "./script.js";
 // Crear una Tabla como album en la base de datos 
 export async function crearTablaAlbum(tableName) {
     // no borrar la tabla principal de las imagenes por accidente 
@@ -68,12 +69,89 @@ export async function mostrarAlbums(){
     
 }
 
+// funcion asincrona para crear el album y esperar la respuesta 
+async function CrearAlbum() {
+  const inputNombre = document.getElementsByClassName("nombre_album_input")[0]
+  // si el campo esta vacío no hacemos nada 
+  if(inputNombre.value ==""){
+    console.log("esta vacío el campo")
+    return
+  }
+
+  // Comenzar la petición de creación de la table Album con los ids seleccionados
+  const idsSeleccionados = obtenerIdsSeleccionados()
+  const nuevoAlbum = await crearTablaAlbum(inputNombre.value) // importante
+
+  // vamos a insertar los valores nuevos a la nueva tabla, los valores de los Id de las imagenes
+  
+
+}
+//Función para obtener lso ids de los seleccionados
+export  function obtenerIdsSeleccionados() {
+  //Seleccionar todas las fotos que tienen clase selcted
+  const elementos = document.getElementsByClassName("selected")
+  const ElementosSeleccionados = Array.from(elementos)
+  const ids = []
+
+  ElementosSeleccionados.forEach((elem)=>{
+    const id = extraerNumeros(elem.id)
+    ids.push(id)
+  })
+  return ids
+}
+
+// funcion para abrir el modal de crear album para el nombre 
+function Mostrar_modal_nombre_album() {
+  // Activar el modal para la captura del nombre 
+  const modal = document.getElementsByClassName("modal_nombre_album")[0]
+  const overlay = document.getElementsByClassName("overlay")[0]
+  modal.style.display="flex"
+  overlay.style.display="block"
+  document.body.style.overflow ="hidden"
+
+}
+// funcion para cerrar el modal de crear album
+function Cerrar_modal_nombre_album(){
+  const modal = document.getElementsByClassName("modal_nombre_album")[0]
+  const overlay = document.getElementsByClassName("overlay")[0]
+  modal.style.display="none"
+  overlay.style.display="none"
+  document.body.style.overflow ="auto"
+}
+
+
+
+
+
+
+
+
 mostrarAlbums()
 
+// Eventos de botones aqui debajo 
 
+// boton de crear un nuevo album
+const BotonCrearAlbum = document.getElementsByClassName("menu_album_createAlbum_option")[0]
+  BotonCrearAlbum.onclick =  ()=>{
+    // verificar que al menos exista un archivo foto seleccionado 
+    if(!(document.getElementsByClassName("selected")[0])){
+      alert("Selecciona al menos una foto")
+      return
+    }
 
+    Mostrar_modal_nombre_album()
+  }
 
+// boton de aceptar del Modal nombre album para crear album 
+const aceptarCrearAlbum = document.getElementsByClassName("aceptarCrearAlbum")[0]
+  aceptarCrearAlbum.onclick = async ()=>{
+    await CrearAlbum()
+  }
 
+const cancelarCrearAlbum = document.getElementsByClassName("cancelarCrearAlbum")[0]
+  cancelarCrearAlbum.onclick =()=>{
+    Cerrar_modal_nombre_album()
+  }
 
 
 
